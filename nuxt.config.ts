27c09f3ea69a9a9342c4c24fe8a7624e1d83290e
Onetro <onetro2024@gmail.com>
2026-08-23 17:36:21 +0100

    fix(deploy): polyfill Set methods and set node 22 in netlify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,32 @@
+// Polyfill ECMAScript 2024 Set methods for environments running on older Node versions
+if (typeof Set.prototype.difference !== 'function') {
+  Set.prototype.difference = function (other) {
+    const result = new Set(this)
+    for (const item of other) {
+      result.delete(item)
+    }
+    return result
+  }
+}
+if (typeof Set.prototype.intersection !== 'function') {
+  Set.prototype.intersection = function (other) {
+    const result = new Set()
+    for (const item of other) {
+      if (this.has(item)) result.add(item)
+    }
+    return result
+  }
+}
+if (typeof Set.prototype.union !== 'function') {
+  Set.prototype.union = function (other) {
+    const result = new Set(this)
+    for (const item of other) {
+      result.add(item)
+    }
+    return result
+  }
+}
+
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
