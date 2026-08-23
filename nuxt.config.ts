27eb787c4796270fee1aad73272c44016fad915d
Onetro <onetro2024@gmail.com>
2026-08-23 17:25:41 +0100

    chore: add netlify nitro preset to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    presets: ['netlify'],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
